21a909ff5d27f28f5964ef44b27fe6344a7ec641
Kostya <kostyakravchenko.dev@gmail.com>
2020-12-06 22:29:01 +0200

    update with regex hw#3

--- homework_3/additional_practice_work_2.py
+++ homework_3/additional_practice_work_2.py
@@ -1,4 +1,5 @@
-import os, re
+import os
+import re
 
 """
 Write a function that reads data from the file,
@@ -11,26 +12,23 @@ if [ERROR] is specified as the logging level.
 The rest of the lines should be ignored.
 """
 
-LOG_FILE = os.path.abspath(r'app.log')
-OUTPUT_FILE = LOG_FILE.split('.')[0] + '_output.txt'
+LOG_FILE = os.path.abspath('app.log')
+OUTPUT_FILE = LOG_FILE.split('.')[0] + '_errors.log'
 LOG_LEVEL = r'[ERROR]'
 
-ip = r'([0-9]{1,3}[\.]){3}[0-9]{1,3}'
+ip = r'[0-9]{1,3}[\.][0-9]{1,3}[\.][0-9]{1,3}[\.][0-9]{1,3}'
 time = r'\d{2}\:\d{2}\:\d{2}\.\d+'
-error = r'\[ERROR\]'
+msg = r'AppLog:[\sa-zA-Z_]*'
 
 
 def parse_log_file(path_to_file):
     with open(path_to_file) as log_file:
         with open(OUTPUT_FILE, 'w') as output_file:
             for row in log_file:
-                if LOG_LEVEL not in row:
-                    continue
-                print(row, end='')
-                row = row.split()
-                output_row = row[0] + ' ' + row[2] + ' ' + ' '.join(row[5::]) + '\n'
-                print(output_row, end='')
-                output_file.write(output_row)
+                if LOG_LEVEL in row:
+                    items = re.compile(ip).findall(row) + re.compile(time).findall(row) + re.compile(msg).findall(row)
+                    # print(' '.join(items))
+                    output_file.write(' '.join(items))
 
 
 parse_log_file(LOG_FILE)
