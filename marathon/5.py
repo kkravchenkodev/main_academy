e659ecbdaca2f6b55aa5d48a9f3fcf3fbc623a36
Kostiantyn_Kravchenko <Kostiantyn_Kravchenko@epam.com>
2021-02-06 18:12:46 +0200

    add 4 and 5 task

--- marathon/5.py
+++ marathon/5.py
@@ -18,39 +18,64 @@ An array of tokes of a valid expression in the standard notation.
 Tokens of the expression in the postfix notation.
 """
 
-import re
-
+# ALGORITHM:
+# 1. Print operands as they arrive.
+# 2. If the stack is empty or contains a left parenthesis on top, push the incoming
+# operator onto the stack.
+# 3. If the incoming symbol is a left parenthesis, push it on the stack.
+# 4. If the incoming symbol is a right parenthesis, pop the stack and print the
+# operators until you see a left parenthesis. Discard the pair of parentheses.
+# 5. If the incoming symbol has higher precedence than the top of the stack, push it
+# on the stack.
+# 6. If the incoming symbol has equal precedence with the top of the stack, use
+# association. If the association is left to right, pop and print the top of the stack and
+# then push the incoming operator. If the association is right to left, push the
+# incoming operator.
+# 7. If the incoming symbol has lower precedence than the symbol on the top of the
+# stack, pop the stack and print the top operator. Then test the incoming operator
+# against the new top of stack.
+# 8. At the end of the expression, pop and print all operators on the stack. (No
+# parentheses should remain.)
 
 def toPostFixExpression(e):
-    # divide string into tokens, and reverse so I can get them in order with pop()
-    # tokens = re.split(r' *([\+\-\*\^/]) *', e)
-    # print(tokens)
-    tokens = [t for t in reversed(e) if t != '']
-    print(tokens)
-    precedence = {'+': 0, '-': 0, '/': 1, '*': 1, '(': 2, ')': 2}
+    class Stack:
+        def __init__(self):
+            self.items = []
+        def isEmpty(self):
+            return self.items == []
+        def push(self, item):
+            self.items.append(item)
+        def pop(self):
+            return self.items.pop()
+        def peek(self):
+            return self.items[len(self.items) - 1]
+        def size(self):
+            return len(self.items)
 
-    # convert infix expression tokens to RPN, processing only
-    # operators above a given precedence
-    def to_rpn(tokens, minprec):
-        rpn = tokens.pop()
-        while len(tokens) > 0:
-            prec = precedence[tokens[-1]]
-            if prec < minprec:
-                break
-            op = tokens.pop()
+    precedence = {'*': 3, '/': 3, '%':3, '+': 2, '-': 2, '(': 1}
+    op_stack = Stack()
+    postfix_list = []
 
-            # get the argument on the operator's right
-            # this will go to the end, or stop at an operator
-            # with precedence <= prec
-            arg2 = to_rpn(tokens, prec + 1)
-            rpn += arg2 + op
-        return rpn
+    for t in e:
+        if t.isdigit():
+            postfix_list.append(t)
+        elif t == '(':
+            op_stack.push(t)
+        elif t == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while (not op_stack.isEmpty()) and (precedence[op_stack.peek()] >= precedence[t]):
+                postfix_list.append(op_stack.pop())
+            op_stack.push(t)
 
-    return list(to_rpn(tokens, 0))
-
-
-
+    while not op_stack.isEmpty():
+        postfix_list.append(op_stack.pop())
+    return postfix_list
 
 
 print(toPostFixExpression(["2", "+", "3"]))
 print(toPostFixExpression(["20", "+", "3", "*", "(", "5", "*", "4", ")"]))
+print(toPostFixExpression(["(", "(", "(", "1", "+", "2", ")", "*", "3", ")", "+", "6", ")", "/", "(", "2", "+", "3", ")"]))
